af200387216b0f53814159466d461d984c99e456
HatemMn <19950216+HatemMn@users.noreply.github.com>
2025-02-13 19:24:04 +0100

    fix: fix

--- crate/cli/src/actions/findex/mod.rs
+++ crate/cli/src/actions/findex/mod.rs
@@ -1,5 +1,4 @@
 pub mod insert_or_delete;
 pub mod parameters;
 pub mod search;
-
-pub(crate) const MAX_SEMAPHORES:usize = 1024; // base value for most linux kernels
+pub(crate) const MAX_SEMAPHORES: usize = 1024; // base value for most linux kernels
