869bb19ee66f0c8de1e059d9bf2bea3fa6714996
HatemMn <19950216+HatemMn@users.noreply.github.com>
2025-02-17 15:29:37 +0100

    fix: 256 permts

--- crate/cli/src/actions/findex/mod.rs
+++ crate/cli/src/actions/findex/mod.rs
@@ -5,5 +5,5 @@ pub mod search;
 ///
 /// Each network call opens a socket which consumes a file descriptor. While the system's file descriptor
 /// limit can be configured (via `ulimit -n` or `/etc/security/limits.conf`), we enforce this fixed limit
-/// which is the default soft limit on many Linux systems to ensure consistent behavior.
-pub(crate) const MAX_PERMITS: usize = 512;
+/// to ensure consistent behavior, but it can be tuned according to the system.
+pub(crate) const MAX_PERMITS: usize = 256;
