cab555dc1642dcf70ea24bdb7ded41b0ad670430
Manuthor <manu.coste@gmail.com>
2025-02-21 14:46:03 +0100

    feat: add FindexParameters constructor

--- crate/cli/src/tests/findex/utils.rs
+++ crate/cli/src/tests/findex/utils.rs
@@ -1,11 +1,8 @@
 use std::{ops::Deref, path::PathBuf};
 
 use cosmian_findex_client::{FindexRestClient, KmsEncryptionLayer, RestClient, RestClientConfig};
-use cosmian_kms_cli::{
-    actions::symmetric::keys::create_key::{CreateKeyAction, SymmetricAlgorithm},
-    reexport::cosmian_kms_client::{
-        reexport::cosmian_http_client::HttpClientConfig, KmsClient, KmsClientConfig,
-    },
+use cosmian_kms_cli::reexport::cosmian_kms_client::{
+    reexport::cosmian_http_client::HttpClientConfig, KmsClient, KmsClientConfig,
 };
 use test_findex_server::start_default_test_findex_server;
 use uuid::Uuid;
@@ -87,7 +84,7 @@ pub(crate) async fn create_encryption_layer<const WORD_LENGTH: usize>(
 ) -> CliResult<KmsEncryptionLayer<WORD_LENGTH, FindexRestClient<WORD_LENGTH>>> {
     let ctx = start_default_test_findex_server().await;
     let kms_client = instantiate_kms_client()?;
-    let findex_parameters = new_parameters(Uuid::new_v4(), &kms_client, true).await?;
+    let findex_parameters = FindexParameters::new(Uuid::new_v4(), &kms_client, true).await?;
 
     let encryption_layer = KmsEncryptionLayer::<WORD_LENGTH, _>::new(
         kms_client.clone(),
@@ -99,52 +96,4 @@ pub(crate) async fn create_encryption_layer<const WORD_LENGTH: usize>(
         ),
     );
     Ok(encryption_layer)
-}
-
-#[allow(clippy::as_conversions)]
-pub(crate) async fn new_parameters(
-    index_id: Uuid,
-    kms_client: &KmsClient,
-    server_side_encryption: bool,
-) -> CliResult<FindexParameters> {
-    async fn generate_key(
-        kms_client: &KmsClient,
-        bits: u32,
-        algorithm: SymmetricAlgorithm,
-        key_type: &str,
-    ) -> CliResult<String> {
-        let uid = CreateKeyAction {
-            number_of_bits: Some(bits as usize),
-            algorithm,
-            ..CreateKeyAction::default()
-        }
-        .run(kms_client)
-        .await?;
-        println!(
-            "Warning: This is the only time that this {key_type} key ID will be printed. ID: {uid}"
-        );
-        Ok(uid.to_string())
-    }
-
-    if server_side_encryption {
-        Ok(FindexParameters {
-            seed_key_id: None,
-            hmac_key_id: Some(
-                generate_key(kms_client, 256, SymmetricAlgorithm::Sha3, "HMAC").await?,
-            ),
-            aes_xts_key_id: Some(
-                generate_key(kms_client, 512, SymmetricAlgorithm::Aes, "AES-XTS").await?,
-            ),
-            index_id,
-        })
-    } else {
-        Ok(FindexParameters {
-            seed_key_id: Some(
-                generate_key(kms_client, 256, SymmetricAlgorithm::Aes, "seed").await?,
-            ),
-            hmac_key_id: None,
-            aes_xts_key_id: None,
-            index_id,
-        })
-    }
 }
