3fee8142247774fca13aba28682b1f2adb1037fe
Manuthor <manu.coste@gmail.com>
2025-02-26 13:34:29 +0100

    chore: move cosmian_findex_client to cli

--- crate/client/src/kms/mod.rs
+++
@@ -1,5 +0,0 @@
-mod encryption_layer;
-mod memory_adt;
-mod requests;
-
-pub use encryption_layer::KmsEncryptionLayer;
--- crate/server/src/database/mod.rs
+++ crate/server/src/database/mod.rs
@@ -1,5 +1,2 @@
 pub(crate) mod database_traits;
 pub mod redis;
-
-#[cfg(test)]
-mod tests;
