c6a76d4af155c088a43b9e0f41b6ef57848b5493
Manuthor <32013169+Manuthor@users.noreply.github.com>
2025-03-27 16:59:21 +0100

    ci: fix cli tests (#66)
    * test: re-enable ignored tests
    
    * chore: debug allocation
    
    * test: debug test_findex_concurrent_read_write
    
    * test: still debugging CI
    
    * fix(http_client): remove keep alive parameter
    
    * fix(http_client): remove keep alive parameter
    
    * fix: add logs on huge allocating size
    
    * fix: add logs on huge allocating size
    
    * chore: update cli submodule
    
    * chore: update submodule cli
    
    * chore: update submodule cli

--- .github/scripts/cargo_build.sh
+++ .github/scripts/cargo_build.sh
@@ -6,6 +6,7 @@ set -ex
 # export TARGET=aarch64-apple-darwin
 # export DEBUG_OR_RELEASE=debug
 # export SKIP_SERVICES_TESTS="--skip test_findex --skip test_all_authentications --skip test_server_auth_matrix --skip test_datasets"
+#
 
 ROOT_FOLDER=$(pwd)
 
@@ -39,6 +40,10 @@ if [ -z "$SKIP_SERVICES_TESTS" ]; then
 fi
 
 rustup target add "$TARGET"
+
+if [ -f /etc/lsb-release ]; then
+  bash .github/scripts/test_utimaco.sh
+fi
 
 # shellcheck disable=SC2086
 cargo build --target $TARGET $RELEASE
