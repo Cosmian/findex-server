3807bd369e1f21ddae5e96238ef93e5dd9b78614
HatemMn <19950216+HatemMn@users.noreply.github.com>
2025-05-16 17:39:49 +0200

    refactor: refactor toml tests

--- crate/server/src/tests/mod.rs
+++ crate/server/src/tests/mod.rs
@@ -1,58 +1,64 @@
-use std::{io::Write, path::PathBuf};
+#[cfg(test)]
+mod tests {
+    use crate::config::{ClapConfig, DBConfig, DatabaseType, HttpConfig, JwtAuthConfig};
+    use std::{io::Write, path::PathBuf};
+    use tempfile::TempDir;
 
-use tempfile::TempDir;
+    fn get_database_configurations() -> (Vec<String>, Vec<DBConfig>) {
+        assert_eq!(
+            DatabaseType::VARIANT_COUNT,
+            2,
+            "If this panic occurs, it means that a new database type has been added. Please update the test to include the new database type."
+        );
 
-use crate::config::{ClapConfig, DBConfig, DatabaseType, HttpConfig, JwtAuthConfig};
+        let db_names = vec!["Redis".to_string(), "Sqlite".to_string()];
 
-#[test]
-fn test_toml() {
-    assert_eq!(
-        DatabaseType::VARIANT_COUNT,
-        2,
-        "If this panic occurs, it means that a new database type has been added. Please update the test to include the new database type."
-    );
-
-    let db_types = vec!["Redis", "Sqlite"];
-
-    let db_configs = vec![
-        DBConfig {
-            database_type: DatabaseType::Redis,
-            database_url: "[some urls]".to_owned(),
-            clear_database: false,
-        },
-        DBConfig {
-            database_type: DatabaseType::Sqlite,
-            database_url: "[some urls]".to_owned(),
-            clear_database: false,
-        },
-    ];
-
-    for (i, db_config) in db_configs.iter().enumerate() {
-        let config = ClapConfig {
-            db: db_config.clone(),
-            http: HttpConfig {
-                port: 443,
-                hostname: "[hostname]".to_owned(),
-                https_p12_file: Some(PathBuf::from("[https p12 file]")),
-                https_p12_password: Some("[https p12 password]".to_owned()),
-                authority_cert_file: Some(PathBuf::from("[authority cert file]")),
+        let db_configs = vec![
+            DBConfig {
+                database_type: DatabaseType::Redis,
+                database_url: "[some urls]".to_owned(),
+                clear_database: false,
             },
-            auth: JwtAuthConfig {
-                jwt_issuer_uri: Some(vec![
-                    "[jwt issuer uri 1]".to_owned(),
-                    "[jwt issuer uri 2]".to_owned(),
-                ]),
-                jwks_uri: Some(vec!["[jwks uri 1]".to_owned(), "[jwks uri 2]".to_owned()]),
-                jwt_audience: Some(vec![
-                    "[jwt audience 1]".to_owned(),
-                    "[jwt audience 2]".to_owned(),
-                ]),
+            DBConfig {
+                database_type: DatabaseType::Sqlite,
+                database_url: "[some urls]".to_owned(),
+                clear_database: false,
             },
-            default_username: "[default username]".to_owned(),
-            force_default_username: false,
-        };
-        let expected_toml = format!(
-            r#"
+        ];
+
+        (db_names, db_configs)
+    }
+
+    #[test]
+    fn test_toml() {
+        let (db_names, db_configs) = get_database_configurations();
+
+        for (i, db_config) in db_configs.iter().enumerate() {
+            let config = ClapConfig {
+                db: db_config.clone(),
+                http: HttpConfig {
+                    port: 443,
+                    hostname: "[hostname]".to_owned(),
+                    https_p12_file: Some(PathBuf::from("[https p12 file]")),
+                    https_p12_password: Some("[https p12 password]".to_owned()),
+                    authority_cert_file: Some(PathBuf::from("[authority cert file]")),
+                },
+                auth: JwtAuthConfig {
+                    jwt_issuer_uri: Some(vec![
+                        "[jwt issuer uri 1]".to_owned(),
+                        "[jwt issuer uri 2]".to_owned(),
+                    ]),
+                    jwks_uri: Some(vec!["[jwks uri 1]".to_owned(), "[jwks uri 2]".to_owned()]),
+                    jwt_audience: Some(vec![
+                        "[jwt audience 1]".to_owned(),
+                        "[jwt audience 2]".to_owned(),
+                    ]),
+                },
+                default_username: "[default username]".to_owned(),
+                force_default_username: false,
+            };
+            let expected_toml = format!(
+                r#"
 default_username = "[default username]"
 force_default_username = false
 
@@ -73,61 +79,57 @@ jwt_issuer_uri = ["[jwt issuer uri 1]", "[jwt issuer uri 2]"]
 jwks_uri = ["[jwks uri 1]", "[jwks uri 2]"]
 jwt_audience = ["[jwt audience 1]", "[jwt audience 2]"]
 "#,
-            db_types[i]
-        );
-        assert_eq!(
-            expected_toml.trim(),
-            toml::to_string(&config).unwrap().trim()
-        );
+                db_names[i]
+            );
+            assert_eq!(
+                expected_toml.trim(),
+                toml::to_string(&config).unwrap().trim()
+            );
+        }
     }
-}
 
-#[test]
-fn test_read_write_toml() {
-    let config = ClapConfig {
-        db: DBConfig {
-            database_type: DatabaseType::Sqlite, // DatabaseType::Redis,
-            database_url: "../../target/debug/sqlite-test.db".to_owned(), // "redis://localhost:6379".to_owned(),
-            clear_database: false,
-        },
-        // db: DBConfig {
-        //     database_type: DatabaseType::Redis,
-        //     database_url: "redis://localhost:6379".to_owned(),
-        //     clear_database: false,
-        // },
-        http: HttpConfig {
-            port: 443,
-            hostname: "[hostname]".to_owned(),
-            https_p12_file: Some(PathBuf::from("[https p12 file]")),
-            https_p12_password: Some("[https p12 password]".to_owned()),
-            authority_cert_file: Some(PathBuf::from("[authority cert file]")),
-        },
-        auth: JwtAuthConfig {
-            jwt_issuer_uri: Some(vec![
-                "[jwt issuer uri 1]".to_owned(),
-                "[jwt issuer uri 2]".to_owned(),
-            ]),
-            jwks_uri: Some(vec!["[jwks uri 1]".to_owned(), "[jwks uri 2]".to_owned()]),
-            jwt_audience: Some(vec![
-                "[jwt audience 1]".to_owned(),
-                "[jwt audience 2]".to_owned(),
-            ]),
-        },
-        default_username: "[default username]".to_owned(),
-        force_default_username: false,
-    };
+    #[test]
+    fn test_read_write_toml() {
+        let (_, db_configs) = get_database_configurations();
 
-    // create a temp dir
-    let tmp_dir = TempDir::new().unwrap();
-    let tmp_path = tmp_dir.path();
-    let tmp_path = tmp_path.join("config.toml");
+        for db_config in db_configs {
+            let config = ClapConfig {
+                db: db_config,
+                http: HttpConfig {
+                    port: 443,
+                    hostname: "[hostname]".to_owned(),
+                    https_p12_file: Some(PathBuf::from("[https p12 file]")),
+                    https_p12_password: Some("[https p12 password]".to_owned()),
+                    authority_cert_file: Some(PathBuf::from("[authority cert file]")),
+                },
+                auth: JwtAuthConfig {
+                    jwt_issuer_uri: Some(vec![
+                        "[jwt issuer uri 1]".to_owned(),
+                        "[jwt issuer uri 2]".to_owned(),
+                    ]),
+                    jwks_uri: Some(vec!["[jwks uri 1]".to_owned(), "[jwks uri 2]".to_owned()]),
+                    jwt_audience: Some(vec![
+                        "[jwt audience 1]".to_owned(),
+                        "[jwt audience 2]".to_owned(),
+                    ]),
+                },
+                default_username: "[default username]".to_owned(),
+                force_default_username: false,
+            };
 
-    let toml_string = toml::to_string(&config).unwrap();
-    let mut file = std::fs::File::create(&tmp_path).unwrap();
-    file.write_all(toml_string.as_bytes()).unwrap();
+            // create a temp dir
+            let tmp_dir = TempDir::new().unwrap();
+            let tmp_path = tmp_dir.path();
+            let tmp_path = tmp_path.join("config.toml");
 
-    let loaded_conf = std::fs::read_to_string(&tmp_path).unwrap();
-    let read_config: ClapConfig = toml::from_str(&loaded_conf).unwrap();
+            let toml_string = toml::to_string(&config).unwrap();
+            let mut file = std::fs::File::create(&tmp_path).unwrap();
+            file.write_all(toml_string.as_bytes()).unwrap();
 
-    assert_eq!(config, read_config);
+            let loaded_conf = std::fs::read_to_string(&tmp_path).unwrap();
+            let read_config: ClapConfig = toml::from_str(&loaded_conf).unwrap();
+
+            assert_eq!(config, read_config);
+        }
+    }
 }
