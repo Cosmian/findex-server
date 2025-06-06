fd721999b13ef4ab3f02b65363ccedd174185d6e
HatemMn <19950216+HatemMn@users.noreply.github.com>
2025-06-06 16:19:31 +0200

    fix: fmt

--- crate/server/src/database/redis/findex.rs
+++ crate/server/src/database/redis/findex.rs
@@ -1,5 +1,7 @@
-use cosmian_findex_memories::RedisMemoryError;
-use cosmian_findex_memories::reexport::cosmian_findex::{Address, MemoryADT};
+use cosmian_findex_memories::{
+    RedisMemoryError,
+    reexport::cosmian_findex::{Address, MemoryADT},
+};
 use cosmian_findex_structs::SERVER_ADDRESS_LENGTH;
 
 use super::Redis;
