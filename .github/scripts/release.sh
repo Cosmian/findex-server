ef664c862669af438b220131e2e70ece6275861e
Emmanuel Coste <emmanuel.coste@cosmian.com>
2025-07-28 13:47:19 +0200

    fix: set versions to help publish afterwards

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -9,6 +9,10 @@ NEW_VERSION="$2"
 SED_BINARY=${SED_BINARY:-sed}
 
 ${SED_BINARY} -i "s/$OLD_VERSION/$NEW_VERSION/g" Cargo.toml
+${SED_BINARY} -i "s/$OLD_VERSION/$NEW_VERSION/g" crate/cli/Cargo.toml
+${SED_BINARY} -i "s/$OLD_VERSION/$NEW_VERSION/g" crate/findex_client/Cargo.toml
+${SED_BINARY} -i "s/$OLD_VERSION/$NEW_VERSION/g" crate/server/Cargo.toml
+${SED_BINARY} -i "s/$OLD_VERSION/$NEW_VERSION/g" crate/test_findex_server/Cargo.toml
 
 # Other files
 ${SED_BINARY} -i "s/$OLD_VERSION/$NEW_VERSION/g" Dockerfile
