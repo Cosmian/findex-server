fe75dc1c9d23312100b50823d5e47e8dd1c560a8
Emmanuel Coste <emmanuel.coste@cosmian.com>
2025-08-11 14:14:18 +0200

    test(docker): fix test script

--- .github/scripts/test_docker_image.sh
+++ .github/scripts/test_docker_image.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
 # Config paths
-CONFIG=~/.cosmian/cosmian.toml
+CONFIG=~/.cosmian/cosmian-no-tls.toml
 TLS_CONFIG=~/.cosmian/cosmian-tls.toml
-FS_URL_HTTP="http://0.0.0.0:6668"
-FS_URL_HTTPS="https://0.0.0.0:6669"
+URL_HTTP="http://0.0.0.0:6668"
+URL_HTTPS="https://0.0.0.0:6669"
+CLI_VERSION="1.2.0"
 
 # Cert paths
-CA_CERT="/home/runner/work/findex-server/findex-server/test_data/client_server/ca/ca.crt"
-CLIENT_CERT="/home/runner/work/findex-server/findex-server/test_data/client_server/owner/owner.client.acme.com.crt"
-CLIENT_KEY="/home/runner/work/findex-server/findex-server/test_data/client_server/owner/owner.client.acme.com.key"
-CLIENT_PKCS12_PATH="/home/runner/work/findex-server/findex-server/test_data/client_server/owner/owner.client.acme.com.p12"
+CA_CERT="test_data/client_server/ca/ca.crt"
+CLIENT_CERT="test_data/client_server/owner/owner.client.acme.com.crt"
+CLIENT_KEY="test_data/client_server/owner/owner.client.acme.com.key"
+CLIENT_PKCS12_PATH="test_data/client_server/owner/owner.client.acme.com.p12"
 
 set -ex
 
-#install cli
+# install cli
 sudo apt update && sudo apt install -y wget
-CLI_VERSION="1.1.0"
 wget "https://package.cosmian.com/cli/$CLI_VERSION/ubuntu-24.04/cosmian-cli_$CLI_VERSION-1_amd64.deb"
 sudo apt install ./"cosmian-cli_$CLI_VERSION-1_amd64.deb"
 cosmian --version
 
 # update cli conf
-sudo mkdir ~/.cosmian
-sudo touch $CONFIG $TLS_CONFIG
-sudo chmod 666 $CONFIG $TLS_CONFIG
-sudo chown root:docker $CONFIG $TLS_CONFIG
+mkdir -p ~/.cosmian
+touch $CONFIG $TLS_CONFIG
+# chmod 666 $CONFIG $TLS_CONFIG
 
 echo '
 [kms_config.http_config]
 server_url = "http://0.0.0.0:9998"
 
 [findex_config.http_config]
-server_url = "'$FS_URL_HTTP'"
-' | sudo tee $CONFIG
+server_url = "'$URL_HTTP'"
+' | tee $CONFIG
 
 echo '
 [kms_config.http_config]
 server_url = "http://0.0.0.0:9998"
 
 [findex_config.http_config]
-server_url = "'$FS_URL_HTTPS'"
+server_url = "'$URL_HTTPS'"
 accept_invalid_certs = true
 ssl_client_pkcs12_path = "'$CLIENT_PKCS12_PATH'"
 ssl_client_pkcs12_password = "password"
-' | sudo tee $TLS_CONFIG
+' | tee $TLS_CONFIG
 
 # Run docker containers
-docker compose -f .github/scripts/docker-compose-authentication-tests.yml up -d
+docker compose -f .github/scripts/docker-compose-authentication-tests.yml up -d --wait
 
 # Wait for the containers to be ready
-sleep 10
+sleep 20
 
 # Function to test OpenSSL connections
 openssl_test() {
@@ -64,8 +63,8 @@ openssl_test() {
 }
 
 # Display server version
-cosmian -c "$CONFIG" --findex-url "$FS_URL_HTTP" findex server-version
-cosmian -c "$TLS_CONFIG" --findex-url "$FS_URL_HTTPS" --findex-accept-invalid-certs findex server-version
+cosmian -c "$CONFIG" findex server-version
+cosmian -c "$TLS_CONFIG" findex server-version
 
 # Test TLS HTTPS server
 openssl_test "127.0.0.1:6669" "tls1_2"
